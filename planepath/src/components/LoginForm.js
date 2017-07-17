425388cf14c47c3aa5a5df360cecaacfa04fde7e
somenugget <some.nugget@gmail.com>
2017-07-17 17:46:19 +0300

    Login frontend. Rewrite login form into class.

--- planepath/src/components/LoginForm.js
+++ planepath/src/components/LoginForm.js
@@ -1,40 +1,52 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
 
-const LoginForm = ({ tryingToLogIn, onSubmit }) => {
-  let usernameInput;
-  let passwordInput;
-  tryingToLogIn = false;
-  const formSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(usernameInput.value, passwordInput.value);
-  };
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.usernameInput = null;
+    this.passwordInput = null;
+    this.formSubmit = this.formSubmit.bind(this);
+  }
 
-  return (
-    <Form onSubmit={formSubmit}>
-      <Form.Field>
-        <label htmlFor="username" >Username</label>
-        <input
-          id="username"
-          placeholder="Username"
-          ref={(node) => { usernameInput = node; }}
-          disabled={tryingToLogIn}
-        />
-      </Form.Field>
-      <Form.Field>
-        <label htmlFor="password" >Password</label>
-        <input
-          id="password"
-          placeholder="Password"
-          ref={(node) => { passwordInput = node; }}
-          disabled={tryingToLogIn}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Button type="submit" color="blue" fluid disabled={tryingToLogIn}>Submit</Button>
-      </Form.Field>
-    </Form>
-  );
-};
+  componentDidMount() {
+    this.usernameInput.focus();
+  }
+
+  formSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.usernameInput.value, this.passwordInput.value);
+  }
+
+  render() {
+    const tryingToLogIn = this.props.tryingToLogIn;
+
+    return (
+      <Form onSubmit={this.formSubmit}>
+        <Form.Field>
+          <label htmlFor="username" >Username</label>
+          <input
+            id="username"
+            placeholder="Username"
+            ref={(node) => { this.usernameInput = node; }}
+            disabled={tryingToLogIn}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="password" >Password</label>
+          <input
+            id="password"
+            placeholder="Password"
+            ref={(node) => { this.passwordInput = node; }}
+            disabled={tryingToLogIn}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Button type="submit" color="blue" fluid disabled={tryingToLogIn}>Submit</Button>
+        </Form.Field>
+      </Form>
+    );
+  }
+}
 
 export default LoginForm;
